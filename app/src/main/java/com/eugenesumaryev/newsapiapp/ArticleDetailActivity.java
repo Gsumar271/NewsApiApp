99ca6c9280225c778297dd78473ca80454b8784c
GeneSumar <statsrats@yahoo.com>
2018-01-15 07:19:36 +0200

    Update ArticleDetailActivity.java

--- app/src/main/java/com/eugenesumaryev/newsapiapp/ArticleDetailActivity.java
+++ app/src/main/java/com/eugenesumaryev/newsapiapp/ArticleDetailActivity.java
@@ -1,4 +1,4 @@
-package com.eugenesumaryev.newsapiapp;
+package com.newsapiapp;
 
 import android.content.Intent;
 import android.os.Bundle;
@@ -9,10 +9,8 @@ import android.support.v4.app.NavUtils;
 import android.view.MenuItem;
 
 /**
- * An activity representing a single Item detail screen. This
- * activity is only used on narrow width devices. On tablet-size devices,
- * item details are presented side-by-side with a list of items
- * in a {@link ArticleMainActivity}.
+ * An activity representing a single screen. This
+ * activity is only used on narrow width devices. 
  */
 public class ArticleDetailActivity extends AppCompatActivity {
 
@@ -22,18 +20,7 @@ public class ArticleDetailActivity extends AppCompatActivity {
         setContentView(R.layout.activity_item_detail);
         Toolbar toolbar = (Toolbar) findViewById(R.id.detail_toolbar);
         setSupportActionBar(toolbar);
-
-        /*
-        FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
-        fab.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                Snackbar.make(view, "Replace with your own detail action", Snackbar.LENGTH_LONG)
-                        .setAction("Action", null).show();
-            }
-        });
-        */
-
+    
         // Show the Up button in the action bar.
         ActionBar actionBar = getSupportActionBar();
         if (actionBar != null) {
@@ -41,15 +28,7 @@ public class ArticleDetailActivity extends AppCompatActivity {
         }
 
 
-        // savedInstanceState is non-null when there is fragment state
-        // saved from previous configurations of this activity
-        // (e.g. when rotating the screen from portrait to landscape).
-        // In this case, the fragment will automatically be re-added
-        // to its container so we don't need to manually add it.
-        // For more information, see the Fragments API guide at:
-        //
-        // http://developer.android.com/guide/components/fragments.html
-        //
+   
         if (savedInstanceState == null) {
             // Create the detail fragment and add it to the activity
             // using a fragment transaction.
@@ -60,8 +39,6 @@ public class ArticleDetailActivity extends AppCompatActivity {
             arguments.putString(ArticleWebFragment.ARG_ITEM_TITLE,
                     getIntent().getStringExtra(ArticleWebFragment.ARG_ITEM_TITLE));
 
-          //  ArticleDetailFragment fragment = new ArticleDetailFragment();
-          //  fragment.setArguments(arguments);
             ArticleWebFragment fragment = new ArticleWebFragment();
             fragment.setArguments(arguments);
             getSupportFragmentManager().beginTransaction()
@@ -75,13 +52,7 @@ public class ArticleDetailActivity extends AppCompatActivity {
     public boolean onOptionsItemSelected(MenuItem item) {
         int id = item.getItemId();
         if (id == android.R.id.home) {
-            // This ID represents the Home or Up button. In the case of this
-            // activity, the Up button is shown. Use NavUtils to allow users
-            // to navigate up one level in the application structure. For
-            // more details, see the Navigation pattern on Android Design:
-            //
-            // http://developer.android.com/design/patterns/navigation.html#up-vs-back
-            //
+  
             NavUtils.navigateUpTo(this, new Intent(this, ArticleMainActivity.class));
             return true;
         }
